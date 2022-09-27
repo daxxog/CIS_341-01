8360f643cd52ad6d1ed0eb5307125a3604a7bfca
Volm, David <david@volminator.com>
2022-09-27 23:29:52 +0000

    use makefile to spawn jobs for cleanup

--- Lab 3 - Create and modify an ASP.NET Core application/CIS341-lab3/scripts/cleanupcode.sh
+++ Lab 3 - Create and modify an ASP.NET Core application/CIS341-lab3/scripts/cleanupcode.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
+_ALL=""
 set -x
+rm cleanupcode.Makefile
 for source_code in $(find . | grep -v './obj' | grep '.cs' | grep -v '.csproj' | grep -v 'wwwroot/lib'); do
-    jb cleanupcode $source_code
+    _ID=$(uuidgen)
+    _ALL="${_ID} ${_ALL}"
+    printf ".PHONY: ${_ID}\n" | tee -a cleanupcode.Makefile
+    printf "${_ID}:\n" | tee -a cleanupcode.Makefile
+    printf "\tjb cleanupcode $source_code\n" | tee -a cleanupcode.Makefile
 done
+
+printf ".PHONY: cleanupcode\n" | tee -a cleanupcode.Makefile
+printf "cleanupcode: ${_ALL}\n" | tee -a cleanupcode.Makefile
+cat cleanupcode.Makefile
+make -f cleanupcode.Makefile cleanupcode -j$(nproc)
