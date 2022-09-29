68ea41e76dcde022f4285316b62581dfb0064384
Volm, David <david@volminator.com>
2022-09-29 00:41:05 +0000

    Middleware lambda thingy

--- Lab 3 - Create and modify an ASP.NET Core application/CIS341-lab3/Program.cs
+++ Lab 3 - Create and modify an ASP.NET Core application/CIS341-lab3/Program.cs
@@ -1,3 +1,5 @@
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,16 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+// https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-6.0
+app.Use(async (context, next) =>
+{
+    // Do work that can write to the Response.
+    await next.Invoke();
+    // Do logging or other work that doesn't write to the Response.
+    Console.WriteLine(
+        $"{DateTime.UtcNow} {context.Request.Scheme}://{context.Request.Host}{context.Request.Path}{context.Request.QueryString}");
+});
 
 // HTTPS redirection is both annoying and unnecessary for local development
 // app.UseHttpsRedirection();
