4f3978a91cdabb2ded8c960f5f4829cbc00f3e7f
Volm, David <david@volminator.com>
2022-10-01 00:34:48 +0000

    modified:   bundle.sh

--- Lab 3 - Create and modify an ASP.NET Core application/bundle.sh
+++ Lab 3 - Create and modify an ASP.NET Core application/bundle.sh
@@ -3,3 +3,5 @@ set -x
 git clean -fdx
 ./rebuildwwwroot.sh
 zip -9r Volm.David.CIS_341-01.Lab-03.zip CIS341-lab3/
+mv Volm.David.CIS_341-01.Lab-03.zip /tmp
+echo rsync -vP daxxog@dxg-linuxbook-btw.local:/tmp/Volm.David.CIS_341-01.Lab-03.zip .
