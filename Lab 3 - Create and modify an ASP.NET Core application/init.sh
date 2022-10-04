7d4141cb8d420d36fb8351d187ba64d48bb5ffbf
Volm, David <david@volminator.com>
2022-10-04 13:44:29 +0000

    modified:   Lab 3 - Create and modify an ASP.NET Core application/init.sh

--- Lab 3 - Create and modify an ASP.NET Core application/init.sh
+++ Lab 3 - Create and modify an ASP.NET Core application/init.sh
@@ -3,7 +3,7 @@ set -x
 PROJECT_NAME="CIS341-lab3"
 dotnet new razor --name "${PROJECT_NAME}"
 cat ./${PROJECT_NAME}/Program.cs | \
-    sed 's/app.UseHttpsRedirection\(\)\;/\/\/ app.UseHttpsRedirection\(\)\; \/\/ nahh/g' | \
+    sed 's/app\.UseHttpsRedirection\(\)\;/\/\/ app\.UseHttpsRedirection\(\)\; \/\/ nahh/g' | \
     tee ./${PROJECT_NAME}/_Program.cs \
     && mv ./${PROJECT_NAME}/_Program.cs ./${PROJECT_NAME}/Program.cs \
 ;
