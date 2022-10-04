98facaa8a888089c91bceb7530dfc563b7efc41e
Volm, David <david@volminator.com>
2022-10-04 13:55:17 +0000

    modified:   bundle.sh

--- Lab 4 - Work with routing and error handling/bundle.sh
+++ Lab 4 - Work with routing and error handling/bundle.sh
@@ -2,6 +2,6 @@
 set -x
 git clean -fdx
 ./rebuildwwwroot.sh
-zip -9r Volm.David.CIS_341-01.Lab-03.zip CIS341-lab3/
-mv Volm.David.CIS_341-01.Lab-03.zip /tmp
-echo rsync -vP daxxog@dxg-linuxbook-btw.local:/tmp/Volm.David.CIS_341-01.Lab-03.zip .
+zip -9r Volm.David.CIS_341-01.Lab-04.zip CIS341-lab4/
+mv Volm.David.CIS_341-01.Lab-04.zip /tmp
+echo rsync -vP daxxog@dxg-linuxbook-btw.local:/tmp/Volm.David.CIS_341-01.Lab-04.zip .
