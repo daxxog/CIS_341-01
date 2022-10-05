7d83ff46b7f32a5da237b64f54b361d296d2ced1
Volm, David <david@volminator.com>
2022-10-05 21:39:46 +0000

    The Thanks page should show the user the following message "Your message was received on [date]." where [date] is the current date and time, and a link that takes the user page to the form.
        The link should be generated using the LinkGenerator service.

--- Lab 4 - Work with routing and error handling/CIS341-lab4/Pages/About/Thanks.cshtml.cs
+++ Lab 4 - Work with routing and error handling/CIS341-lab4/Pages/About/Thanks.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +6,18 @@ namespace CIS341_lab4.Pages
 {
     public class ThanksModel : PageModel
     {
+        private readonly LinkGenerator _linkGenerator;
+
+        public ThanksModel(LinkGenerator linkGenerator)
+        {
+            _linkGenerator = linkGenerator;
+        }
+
         public void OnGet()
         {
+            string dateTime = DateTime.Now.ToString("r", new CultureInfo("en-US"));
+            ViewData["TimeStamp"] = dateTime;
+            ViewData["ContactFormPath"] = _linkGenerator.GetPathByPage("/About/Contact");
         }
     }
 }
