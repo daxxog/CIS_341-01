aea904cb4fcd989c24a4da3eafff0b437a8452e6
Volm, David <david@volminator.com>
2022-10-22 17:48:31 +0000

    modified:   bundle.sh

--- Lab 5 - Implement Controller structure/bundle.sh
+++ Lab 5 - Implement Controller structure/bundle.sh
@@ -2,6 +2,6 @@
 set -x
 git clean -fdx
 ./rebuildwwwroot.sh
-zip -9r Volm.David.CIS_341-01.Lab-04.zip CIS341-lab4/
-mv Volm.David.CIS_341-01.Lab-04.zip /tmp
-echo rsync -vP daxxog@dxg-linuxbook-btw.local:/tmp/Volm.David.CIS_341-01.Lab-04.zip .
+zip -9r Volm.David.CIS_341-01.Lab-05.zip CIS341-lab5/
+mv Volm.David.CIS_341-01.Lab-05.zip /tmp
+echo rsync -vP daxxog@dxg-linuxbook-btw.local:/tmp/Volm.David.CIS_341-01.Lab-05.zip .
