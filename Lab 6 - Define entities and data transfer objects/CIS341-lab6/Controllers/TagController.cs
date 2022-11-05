9287e18757fd34652ffccf64e74789947549a5f5
Volm, David <david@volminator.com>
2022-11-05 18:57:54 +0000

    pull tags from database and do a very simple weighting

--- Lab 6 - Define entities and data transfer objects/CIS341-lab6/Controllers/TagController.cs
+++ Lab 6 - Define entities and data transfer objects/CIS341-lab6/Controllers/TagController.cs
@@ -1,39 +1,78 @@
+using Newtonsoft.Json;
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CIS341_lab6.Data;
+using CIS341_lab6.Data.Entities;
 using CIS341_lab6.Models;
 
 namespace CIS341_lab6.Controllers
 {
     public class TagController : Controller
     {
-        public TagController()
+        private readonly SqliteContext _context;
+
+        public TagController(SqliteContext context)
         {
+            _context = context;
         }
 
         // GET: Tags
         [Route("/Tags")]
         public async Task<IActionResult> Index()
         {
-            IEnumerable<WeightedTagModel> tags = new WeightedTagModel[]
+            if (_context.TaggedInformationItems == null)
             {
-                new WeightedTagModel
+                return Problem("Entity set 'SqliteContext.TaggedInformationItems'  is null.");
+            }
+
+            IEnumerable<TaggedInformationItem> taggedInformationItems =
+                await _context.TaggedInformationItems.ToListAsync();
+            Dictionary<String, WeightedTagModel> weightedTags = new Dictionary<String, WeightedTagModel>();
+
+            // count the tags
+            foreach (TaggedInformationItem taggedInformationItem in taggedInformationItems)
+            {
+                string tagName = taggedInformationItem.TagName;
+                WeightedTagModel weightedTag;
+                if (!weightedTags.TryGetValue(tagName, out weightedTag))
                 {
-                    TagName = "sausage",
-                    Weight = 5,
-                },
-                new WeightedTagModel
+                    weightedTag = new WeightedTagModel
+                    {
+                        TagName = tagName,
+                        Weight = 0,
+                    };
+                    weightedTags.Add(tagName, weightedTag);
+                }
+
+                weightedTag.Weight++;
+            }
+
+            // sort tags by name and invert the weights (so they map to h tags used in the view)
+            IEnumerable<WeightedTagModel> tags = weightedTags.Values.OrderBy(weightedTag =>
+            {
+                // sort by tag name
+                return weightedTag.TagName;
+            }).Select(weightedTag =>
+            {
+                // smaller number is better weight (because these map to h tags)
+                weightedTag.Weight = 7 - weightedTag.Weight;
+
+                // there is a better way to implement this with larger datasets
+                // we also could do a lot of this in SQL, but to quote Professor Heimonen:
+                //
+                // "This isn't a database class."
+                //
+                if (weightedTag.Weight < 1)
                 {
-                    TagName = "pork shoulder",
-                    Weight = 2,
-                },
-                new WeightedTagModel
-                {
-                    TagName = "bacon",
-                    Weight = 4,
-                },
-            };
+                    weightedTag.Weight = 1;
+                }
+
+                return weightedTag;
+            }).AsEnumerable();
+
             return View(tags);
         }
 
