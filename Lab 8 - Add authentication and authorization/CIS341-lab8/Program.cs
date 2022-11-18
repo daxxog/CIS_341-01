208c2c61ed5d2982f8b4784d1a48da65a8836c8d
Volm, David <david@volminator.com>
2022-11-18 23:57:09 +0000

    Configure the password policy and change the options to be more secure

--- Lab 8 - Add authentication and authorization/CIS341-lab8/Program.cs
+++ Lab 8 - Add authentication and authorization/CIS341-lab8/Program.cs
@@ -53,6 +53,19 @@ using (var _context = new SqliteContext(Microsoft.EntityFrameworkCore.SqliteDbCo
     }
 }
 
+
+// https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity-configuration?view=aspnetcore-6.0#password
+builder.Services.Configure<IdentityOptions>(options =>
+{
+    // Password settings.
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 24;
+    options.Password.RequiredUniqueChars = 12;
+});
+
 // https://learn.microsoft.com/en-us/aspnet/core/fundamentals/middleware/?view=aspnetcore-6.0
 app.Use(async (context, next) =>
 {
