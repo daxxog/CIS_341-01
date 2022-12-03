c1875d993cce011e09988c240cd8a4da344697a3
Volm, David <david@volminator.com>
2022-12-03 23:57:37 +0000

    remove global.json from zip file

--- Checkpoint 2/bundle.sh
+++ Checkpoint 2/bundle.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -x
 git clean -fdx
+rm ./CIS341-checkpoint2/global.json
 ./rebuildwwwroot.sh
 zip -9r Volm.David.CIS_341-01.Checkpoint-2.zip CIS341-checkpoint2/
+git restore ./CIS341-checkpoint2/global.json
 mv Volm.David.CIS_341-01.Checkpoint-2.zip /tmp
 echo rsync -vP daxxog@dxg-linuxbook-btw.local:/tmp/Volm.David.CIS_341-01.Checkpoint-2.zip .
